20e1cc63505d2560a0f59e84127d9957e96098bc
Hustlekit <ceo@christopherhustle.com>
2025-08-02 22:14:55 +0200

    init

---
+++ src/test/java/com/hastesoft/ghrepo/GhrepoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hastesoft.ghrepo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GhrepoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
